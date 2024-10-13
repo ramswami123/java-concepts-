0a170109176000c99d44bf85f3b6445554ba30d3
Geeta Krishna <75007299+ramswami123@users.noreply.github.com>
2024-10-13 20:29:20 +0530

    Create Arraylistcollection.java

---
+++ Arraylistcollection.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Arraylistcollection {
+    public static void main(String[] args) {
+      List<Integer> vector = new ArrayList<Integer>();
+      
+      for(int i=0;i<10;i++){
+        vector.add(i);
+      }
+      
+      System.out.println(vector);
+  }
+}
